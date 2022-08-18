a63f5d3b5a9fd60bbd11687c1f1a8a3120649665
Wing Chau <wing@devtography.com>
2022-08-18 04:16:07 +0100

    Add util `node-env`

---
+++ src/utils/node-env.ts
@@ -0,0 +1,14 @@
+/**
+ * Predefined NODE_ENV values
+ */
+
+/** `NODE_ENV=production` */
+export const prod = process.env.NODE_ENV === 'production';
+
+/** `NODE_ENV=development` */
+export const dev = process.env.NODE_ENV === 'development';
+
+/** `NODE_ENV=test` */
+export const test = process.env.NODE_ENV === 'test';
+
+export default { prod, dev, test };
